07d031c93de78ea938a9106a29aa1b2c841ff224
Ariel <arielgomezmendez94@gmail.com>
2023-02-15 10:39:12 -0500

    Create the Search component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Card from "./components/Card/Card";
+import Search from "./components/Search/Search";
 
 function App() {
   return (
     <div className="App">
+      <Search />
       <Card />
     </div>
   );
